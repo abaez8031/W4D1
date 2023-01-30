f40be55d1c08770fabf62445ea2bea94d55b2085
abaez8031 <abaez8031@gmail.com>
2023-01-30 11:14:15 -0500

    first commit

---
+++ KnightPathFinder.rb
@@ -0,0 +1,17 @@
+class KnightPathFinder
+
+  def self.build_move_tree()
+    
+  end
+
+
+  def initialize(value)
+    @value = value
+    @possible_moves = []
+    @root_node = PolyTreeNode.new(value)
+  end
+
+
+
+
+end
